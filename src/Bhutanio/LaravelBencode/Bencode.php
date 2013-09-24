dd769ce5f72f9d1f43f8ec8d1f532f46b2c36b6a
Abi Narayan <abixalmon@hotmail.com>
2013-09-24 19:08:16 +0600

    initial commit
    initial commit

--- src/Bhutanio/LaravelBencode/Bencode.php
+++ src/Bhutanio/LaravelBencode/Bencode.php
@@ -2,7 +2,7 @@
 namespace Bhutanio\BEncode;
 
 /**
- * A Laravel wrapper for BEncode
+ * A BEncode Wrapper for Laravel
  * Original src - https://wiki.theory.org/Decoding_encoding_bencoded_data_with_PHP
  * 
  * @package laravel-bencode
