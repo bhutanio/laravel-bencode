967f4ea49c2f9910d9cf96cd3743ba939a002b10
Abi Narayan <abixalmon@hotmail.com>
2013-09-24 18:58:45 +0600

    initial commit
    initial commit

---
+++ src/Bhutanio/LaravelBencode/Bencode.php
@@ -0,0 +1,13 @@
+<?php
+namespace Bhutanio\BEncode;
+
+/**
+ * A Laravel wrapper for BEncode
+ * Original src - https://wiki.theory.org/Decoding_encoding_bencoded_data_with_PHP
+ * 
+ * @package laravel-bencode
+ * @author Abi Xalmon
+ */
+class BENCODE {
+
+}
---
+++ src/Bhutanio/LaravelBencode/ServiceProvider.php
@@ -0,0 +1,48 @@
+<?php
+namespace Bhutanio\BEncode;
+
+use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
+
+class ServiceProvider extends IlluminateServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = true;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('bhutanio/laravel-bencode');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app['bencode'] = $this->app->share(function($app)
+			{
+				return new BENCODE;
+			});
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('bencode');
+	}
+
+}
